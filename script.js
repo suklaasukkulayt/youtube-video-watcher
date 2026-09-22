5708d0eada0da14be8a0a88fd11348dfd72d0fbe
suklaasukkulayt <suklaasukkulayt@gmail.com>
2026-09-22 06:58:04 +0000

    added functionality

---
+++ script.js
@@ -0,0 +1,22 @@
+function showInfo(){
+    const infoDiv = document.getElementById("info");
+    if (infoDiv.style.display === "none"){
+        infoDiv.style.display = "flex";
+    } else {
+        infoDiv.style.display = "none";
+    }   
+}
+
+const youtubeVideoIdForm = document.getElementById("youtubeVideoIdForm");
+youtubeVideoIdForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let videoIdValue = document.getElementById('videoId').value;
+    var url = videoIdValue;
+    let videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+if (videoid != null) {
+    document.getElementById("youtubeplayer").src = "https://www.youtube-nocookie.com/embed/" + videoid[1];
+} else {
+    document.getElementById("youtubeplayer").src = "https://www.youtube-nocookie.com/embed/" + videoIdValue;
+}
+    
+})
