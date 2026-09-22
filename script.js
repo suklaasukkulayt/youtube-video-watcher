20390b97dbef4017179890800b112fe2c28fdc99
suklaasukkulayt <suklaasukkulayt@gmail.com>
2026-09-22 07:01:01 +0000

    added autoplay

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ youtubeVideoIdForm.addEventListener('submit', function(event) {
     var url = videoIdValue;
     let videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
 if (videoid != null) {
-    document.getElementById("youtubeplayer").src = "https://www.youtube-nocookie.com/embed/" + videoid[1];
+    document.getElementById("youtubeplayer").src = "https://www.youtube-nocookie.com/embed/" + videoid[1] + "?autoplay=1";
 } else {
-    document.getElementById("youtubeplayer").src = "https://www.youtube-nocookie.com/embed/" + videoIdValue;
+    document.getElementById("youtubeplayer").src = "https://www.youtube-nocookie.com/embed/" + videoIdValue + "?autoplay=1";
 }
     
 })
